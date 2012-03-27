de206e9bba19d23ee489d448da7337d6e97d0f44
James Sullins <jcsullins@gmail.com>
2012-03-27 17:59:03 -0500

    rm 12781/1 (no longer needed)

--- apply-cherry_picks.sh
+++ apply-cherry_picks.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# hax for tenderloin (hardware/qcom/display)
-echo ">>> START 12781/1"
-pushd hardware/qcom/display
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_hardware_qcom_display refs/changes/81/12781/1 && git cherry-pick FETCH_HEAD
-popd
-echo ">>> END 12781/1"
-echo ""
-
 # libcutils/android_reboot: tenderloin hax to use HPalm restart_reasons
 echo ">>> START 12782/1"
 pushd system/core
